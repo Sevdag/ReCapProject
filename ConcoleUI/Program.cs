93dac1fc12897236dc05ff4a804155de0f4e8963
S_Gunes <gunesevdaa@gmail.com>
2021-02-13 20:39:50 +0200

    EntityFramework
    130221

--- ConcoleUI/Program.cs
+++ ConcoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Bussiness.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using System;
@@ -9,7 +10,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            CarManager carManager = new CarManager(new EfCarDal());
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.BrandId);
